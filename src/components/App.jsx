9a98c8476160710dc414af232667782405ded45e
Jonatas <jonatasalmeidasm@gmail.com>
2022-10-20 18:35:19 -0300

    timeline is now re-rendering posts when an user make a new publication (there's a delay caused by url-metadata)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import styled from "styled-components";
 
 import GlobalStyle from "../styles/GlobalStyles";
 import UserContext from "../contexts/userContext";
-
+import ReloadContext from "../contexts/reloadContext";
+import PostsContext from "../contexts/postsContext";
 import Feed from "./Feed";
 
 import Alert from "./Common/Alert";
@@ -15,34 +16,47 @@ import SignIn from "./Screens/SignIn";
 export default function App() {
   const [userData, setUserData] = useState({});
   const [alert, setAlert] = useState({});
+  const [reload, setReload] = useState(false);
+  const [arrPosts, setArrPosts] = useState([]);
 
   return (
     <>
       <GlobalStyle />
-      <UserContext.Provider
-        value={{
-          userData,
-          setUserData,
-          alert,
-          setAlert,
-        }}
-      >
-        <Container>
-          {alert.show && <Alert />}
-          <BrowserRouter>
-            <Routes>
-
-              <Route path="/" element={<SignIn />}></Route>
-              <Route path="/sign-up" element={<SignUp />}></Route>
-              <Route path="/feed" element={<Feed type={'timeline'}/>}></Route>
-              <Route path="/hashtag/:hashtag" element={<Feed type={'hashtag'}/>}></Route>
-              <Route path="/users/:id" element={<Feed type={'user'} />}></Route>
-              <Route path="*" element={<Navigate to="/" />}></Route>
-              
-            </Routes>
-          </BrowserRouter>
-        </Container>
-      </UserContext.Provider>
+      <ReloadContext.Provider value={{ reload, setReload }}>
+        <UserContext.Provider
+          value={{
+            userData,
+            setUserData,
+            alert,
+            setAlert,
+          }}
+        >
+          <PostsContext.Provider value={{ arrPosts, setArrPosts }}>
+            <Container>
+              {alert.show && <Alert />}
+              <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<SignIn />}></Route>
+                  <Route path="/sign-up" element={<SignUp />}></Route>
+                  <Route
+                    path="/feed"
+                    element={<Feed type={"timeline"} />}
+                  ></Route>
+                  <Route
+                    path="/hashtag/:hashtag"
+                    element={<Feed type={"hashtag"} />}
+                  ></Route>
+                  <Route
+                    path="/users/:id"
+                    element={<Feed type={"user"} />}
+                  ></Route>
+                  <Route path="*" element={<Navigate to="/" />}></Route>
+                </Routes>
+              </BrowserRouter>
+            </Container>
+          </PostsContext.Provider>
+        </UserContext.Provider>
+      </ReloadContext.Provider>
     </>
   );
 }
