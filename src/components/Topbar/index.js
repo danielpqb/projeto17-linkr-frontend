6ceff77ab2fc666e7b395f1f070ceaf09b56fd7d
danielpqb <danielpqb@gmail.com>
2022-10-21 19:33:32 -0300

    Drop down menu

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { FiChevronDown } from 'react-icons/fi';
+import { FiChevronDown } from "react-icons/fi";
 
 import { Container, UserLogo } from "./style";
 import UserContext from "../../contexts/userContext";
 import SearchBar from "./Searchbar/index";
 
 export default function TopBar() {
-    const { userData } = useContext(UserContext);
-    const imgSrc = userData.imageUrl;
-    const navigate = useNavigate();
+  const { userData } = useContext(UserContext);
+  const [isMenuAppering, setIsMenuAppering] = useState();
 
-    return (
-        <Container>
-            <h1 onClick={() => navigate('/timeline')}>linkr</h1>
-            <SearchBar />
-            <UserLogo>
-                <FiChevronDown />
-                <img src={imgSrc} alt="" />
-            </UserLogo>
-        </Container>
-    )
-};
+  const imgSrc = userData.imageUrl;
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <h1 onClick={() => navigate("/timeline")}>linkr</h1>
+      <SearchBar />
+      <UserLogo
+        onClick={() => {
+          setIsMenuAppering(!isMenuAppering);
+          console.log(isMenuAppering);
+        }}
+      >
+        <FiChevronDown />
+        <img src={imgSrc} alt="" />
+      </UserLogo>
+    </Container>
+  );
+}
