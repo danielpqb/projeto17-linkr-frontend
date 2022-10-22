a09f6c2ee6b19c1cbd11bd1e83b9434dfd0e88f9
g-rmc <g.rmc3000@gmail.com>
2022-10-21 23:21:48 -0300

    Set edit/delete buttons to user's posts only

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,21 +1,36 @@
 import React from "react";
+import { useState, useContext } from "react";
 import { TiPencil, TiTrash } from "react-icons/ti";
 
+import UserContext from "../../contexts/userContext";
 import { Container, MetadataContent, MetadataDiv, MetadataLink, MetadataText, MetadataTitle, PostText, PostUserName } from "./style";
 
-export default function Post({ userImage, userName, postText, metadata, postLink, postId}) {
+export default function Post({ userId, userImage, userName, postText, metadata, postLink, postId}) {
+    const { userData } = useContext(UserContext);
+    const [ isEditing, setIsEditing ] = useState(false);
+
+    const isEditable = userData.id === userId;
+
     return (
         <Container>
             <img src={userImage} alt='User profile image'/>
             <div>
                 <PostUserName>
                     {userName}
-                    <div>
-                        <TiPencil />
-                        <TiTrash onClick={() => alert(`Aqui vai deletar o post id:${postId}!`)}/>
-                    </div>
+                    {isEditable? 
+                        <div>
+                            <TiPencil onClick={() => setIsEditing(!isEditing)}/>
+                            <TiTrash onClick={() => alert(`Aqui vai deletar o post id:${postId}!`)}/>
+                        </div>
+                        :
+                        <></>
+                    }
                 </PostUserName>
-                <PostText>{postText}</PostText>
+                {isEditing?
+                    <PostText>Ôvoeditah se for o userId{userId}</PostText>
+                    :
+                    <PostText>{postText}</PostText>
+                }
                 <MetadataDiv onClick={()=>{window.open(postLink)}}>
                     <MetadataContent>
                         <MetadataTitle>{metadata.title}</MetadataTitle>
