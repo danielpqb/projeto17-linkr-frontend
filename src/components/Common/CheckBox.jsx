a4b82babf0dbcb6640bd57c0029045adb02c0722
danielpqb <danielpqb@gmail.com>
2022-10-24 23:28:31 -0300

    Show password

---
+++ src/components/Common/CheckBox.jsx
@@ -0,0 +1,27 @@
+import { BiCheckbox, BiCheckboxSquare } from "react-icons/bi";
+import styled from "styled-components";
+
+export default function CheckBox({ size, color, isChecked, setIsChecked }) {
+  color = color ? color : "rgb(70, 70, 70)";
+
+  return (
+    <Container
+      size={size}
+      isChecked={isChecked}
+      onClick={() => {
+        setIsChecked(!isChecked);
+        console.log(isChecked);
+      }}
+    >
+      {isChecked ? <BiCheckboxSquare color={color} size={size} /> : <BiCheckbox color={color} size={size} />}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  & {
+    display: flex;
+
+    margin: 0px 10px;
+  }
+`;
