428bdb50718ff4e24add73beaf3ab3e6fb2368a1
danielpqb <danielpqb@gmail.com>
2022-10-25 20:05:15 -0300

    Fixing URL regex

--- src/components/Publish/index.js
+++ src/components/Publish/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Swal from "sweetalert2";
-import {
-  createPost,
-  createHashtag,
-  createPostsHashtags,
-  getTrendingHashtags,
-} from "../../services/linkrAPI";
+import { createPost, createHashtag, createPostsHashtags, getTrendingHashtags } from "../../services/linkrAPI";
 import { getTimelinePosts } from "../../services/linkrAPI";
 import PostsContext from "../../contexts/postsContext";
 import UserContext from "../../contexts/userContext";
 import { Container, LeftDiv, Form, Input, Button, RightDiv } from "./style";
-import checkHashtags from '../functions/checkHashtags';
+import checkHashtags from "../functions/checkHashtags";
+import { regexPatterns } from "../../constants/regexPatterns";
 
 export default function Publish() {
   const [form, setForm] = React.useState({ link: "", text: "" });
@@ -19,9 +15,7 @@ export default function Publish() {
   const { userData } = React.useContext(UserContext);
 
   function validateUrl(value) {
-    return /^(?:(?:(?:https?|http):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
-      value
-    );
+    return regexPatterns.url.test(value);
   }
 
   function handleForm(e) {
@@ -90,8 +84,9 @@ export default function Publish() {
 
     getTrendingHashtags()
       .then((answer) => {
-          setArrTrendingHashtags(answer.data[0]);
-      }).catch((res) => {
+        setArrTrendingHashtags(answer.data[0]);
+      })
+      .catch((res) => {
         Swal.fire(res.response.data.message);
       });
 
@@ -109,32 +104,15 @@ export default function Publish() {
 
         {isLoading ? (
           <Form>
-            <Input
-              disabled
-              placeholder="Links que cabem no bolso"
-              name="link"
-              type="text"
-            />
-            <Input
-              disabled
-              placeholder="Awesome article about #javascript"
-              name="text"
-              type="text"
-            />
+            <Input disabled placeholder="Links que cabem no bolso" name="link" type="text" />
+            <Input disabled placeholder="Awesome article about #javascript" name="text" type="text" />
             <Button disabled type="submit">
               Publishing...
             </Button>
           </Form>
         ) : (
           <Form onSubmit={publishPost}>
-            <Input
-              placeholder="http:// ..."
-              name="link"
-              type="text"
-              value={form.link}
-              onChange={handleForm}
-              required
-            />
+            <Input placeholder="http:// ..." name="link" type="text" value={form.link} onChange={handleForm} required />
             <Input
               placeholder="Awesome article about #javascript"
               name="text"
--- src/constants/regexPatterns.js
+++ src/constants/regexPatterns.js
@@ -2,7 +2,7 @@ const regexPatterns = {
   name: /^[a-z0-9]{3,20}$/i,
   email: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i,
   password: /^(?=.*[0-9])(?=.*[a-z]).{8,32}/i,
-  url: /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/,
+  url: /^http(s):\/\/.*/,
 };
 
 export { regexPatterns };
