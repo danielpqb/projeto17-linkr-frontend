98dfd87abc7dea23445354255708d0d72cf5fd23
g-rmc <g.rmc3000@gmail.com>
2022-10-25 21:29:54 -0300

    Updates for new Url Database

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -155,7 +155,7 @@ export default function Feed({ type }) {
                           userName={post.user.name}
                           postText={post.text}
                           metadata={post.metadata}
-                          postLink={post.link}
+                          postLink={post.metadata.link}
                           postId={post.id}
                         />
                       ))}
